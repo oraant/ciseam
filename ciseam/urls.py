86e7d643d98cb4d1cfd5cab9564d448f699f54b5
oraant <oraant777@gmail.com>
2015-12-23 16:29:33 +0800

    Add index page for test.

--- ciseam/urls.py
+++ ciseam/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from eam import views as eam_v
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', eam_v.index),
 ]
--- eam/views.py
+++ eam/views.py
@@ -1,3 +1,7 @@
+#coding:utf-8
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse(u"北京中研软科技有限公司 资产管理系统")
