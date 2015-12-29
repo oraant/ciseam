11ffb006e70f525ebaa8b5b81e163a9aad700aea
oraant <oraant777@gmail.com>
2015-12-29 17:56:48 +0800

    Add asset_mark in table,Modify construct of functions,Add <a> tag in html page.

--- eam/views.py
+++ eam/views.py
@@ -24,7 +24,7 @@ def index(request):
 #------------------------------------------------------------------------------------------------------------------
 #http://192.168.18.132:8000/filter_user/?id=&user_name=&tel=&qq=&email=&user_comment=
 def filter_user(req):
-    column=['ID', '姓名', '电话', 'QQ', '邮箱', '备注']
+    column=['用户ID', '姓名', '电话', 'QQ', '邮箱', '备注']
 
     sql='select id,user_name,tel,qq,email,user_comment from eam_user'
     data=filter_data(sql,req)
@@ -46,10 +46,11 @@ def delete_user(req):
     sql = 'delete from eam_user'
     msg = delete_data(sql,req)
     return HttpResponse(msg)
+
 #------------------------------------------------------------------------------------------------------------------
 #http://192.168.18.132:8000/filter_asset/?id=&asset_mark=&asset_name=&intake_date=&warranty_period=&price=
 def filter_asset(req):
-    column=['ID', '编号', '名称', '购买日期', '保修期', '价格']
+    column=['资产ID', '资产编号', '资产名称', '购买日期', '保修期', '价格']
 
     sql='select id,asset_mark,asset_name,intake_date,warranty_period,price from eam_asset'
     data=filter_data(sql,req)
@@ -75,8 +76,13 @@ def delete_asset(req):
 #------------------------------------------------------------------------------------------------------------------
 #http://192.168.18.132:8000/filter_attributes/?id=&asset_id=&attribute_key=&attribute_value
 def filter_attributes(req):
+    column=['资产ID', '资产名称', '资产属性', '资产属性值']
+
     sql='select b.id,b.asset_name,a.attribute_key,a.attribute_value from eam_attributes a join eam_asset b on a.asset_id=b.id'
-    data = filter_data(sql,req,True)
+    data = filter_data(sql,req)
+
+    table = format_table(column,data)
+    return render(req, 'show_table.html', table)
 
 def update_attributes(req):
     sql='update eam_attributes'
@@ -92,12 +98,13 @@ def delete_attributes(req):
     sql = 'delete from eam_attributes'
     msg = delete_data(sql,req)
     return HttpResponse(msg)
-#------------------------------------------------------------------------------------------------------------------
-#http://192.168.18.132:8000/filter_maintenance/?id=&asset_id=&user_id=&fault_cause=&occur_date=&repair_date=&repair_result=
-def filter_maintenance(req):
-    column=['ID', '责任人', '设备', '故障原因', '故障出现日期', '故障修复日期', '故障修复结果']
 
-    sql='''select a.id,b.user_name,c.asset_name,a.fault_cause,a.occur_date,a.repair_date,a.repair_result
+#------------------------------------------------------------------------------------------------------------------
+#http://192.168.18.132:8000/filter_maintenance/?id=&asset_mark=&asset_name=&user_name=&fault_cause=&occur_date=&repair_date=&repair_result=
+def filter_maintenance(req):
+    column=['ID', '责任人', '设备名称', '设备编号', '故障原因', '故障出现日期', '故障修复日期', '故障修复结果']
+
+    sql='''select a.id,b.user_name,c.asset_name,c.asset_mark,a.fault_cause,a.occur_date,a.repair_date,a.repair_result
            from eam_maintenance a left join eam_user b on (a.user_id = b.id) join eam_asset c on (a.asset_id = c.id)'''
     data = filter_data(sql,req)
 
@@ -118,11 +125,16 @@ def delete_maintenance(req):
     sql = 'delete from eam_maintenance'
     msg = delete_data(sql,req)
     return HttpResponse(msg)
+
 #------------------------------------------------------------------------------------------------------------------
-#http://192.168.18.132:8000/filter_usagerecord?id=&asset_id=&user_id=&begin_date=&end_date
+#http://192.168.18.132:8000/filter_usagerecord?id=&asset_name=&asset_mark&user_name=&begin_date=&end_date
 def filter_usagerecord(req):
-    sql='select * from eam_usagerecord a left join eam_user b on (a.user_id = b.id) join eam_asset c on (a.asset_id = c.id)'
-    return filter_data(sql,req)
+    column=['记录ID', '设备名称', '设备编号', '使用用户', '开始使用日期', '归还日期']
+    sql='select a.id,c.asset_name,c.asset_mark,b.user_name,a.begin_date,a.end_date from eam_usagerecord a left join eam_user b on (a.user_id = b.id) join eam_asset c on (a.asset_id = c.id)'
+    data = filter_data(sql,req)
+
+    table = format_table(column,data)
+    return render(req, 'show_table.html', table)
 
 def update_usagerecord(req):
     sql = 'update eam_usagerecord'
@@ -138,6 +150,7 @@ def delete_usagerecord(req):
     sql = 'delete from eam_usagerecord'
     msg = delete_data(sql,req)
     return HttpResponse(msg)
+
 #------------------------------------------------------------------------------------------------------------------
 #format sql with where condition,and fetch data
 def filter_data(sql,req,exact=False):#don't forget try..catch..else
@@ -159,77 +172,42 @@ def filter_data(sql,req,exact=False):#don't forget try..catch..else
 def update_data(sql,req):#id is not null
     try:
         id = req.GET['id']
-    except：
-        return '没有指定要删除的ID'
+    except:
+        return '没有指定要更新的ID'
 
-    request = dict(filter(lambda x: x[1] != '' and x[0] != 'id', req.GET.items()))
-    changes = ' and '.join(map(lambda x:'%s="%s"'%(x[0],x[1]),request.items()))
+    request = dict(filter(lambda x: x[1] != '' and x[0] != 'id', req.GET.items()))#去掉空参数，和名为ID的参数
+    changes = ','.join(map(lambda x:'%s="%s"'%(x[0],x[1]),request.items()))#排列要更新的列和值
+
     sql = sql + ' set %s where id=%s'%(changes,id)
+    return execute(sql)
 
-    try:
-        cursor.execute(sql)
-        conn.commit()
-        rowcount = cursor.rowcount
-    except Exception as e:
-        print sql
-        print e
-        rowcount = -1
-
-    if rowcount == 0:
-        return '没有找到对应的行'
-    elif rowcount == -1:
-        return '更新失败'
-    else:
-        return '更新成功'
-
+#make sure the not null column is not null
 def insert_data(sql,req):
-    #request = dict(filter(lambda x: x[1] != '', req.GET.items()))
-    keys = ','.join(request.keys())#排列有哪些列
+    request = req.GET
+    keys = ','.join(request.keys())#排列插入时有哪些列的值
     values = map(lambda x:'"'+x+'"' if isinstance(x, unicode) else '"'+str(x)+'"',request.values())#把unicode值和int值转成带双引号的字符串
     values = map(lambda x:'NULL' if x=='""' else x,values)#把空值转换为不带引号的NULL值
     values = ','.join(values)
 
     sql = sql + '(%s) values(%s)'%(keys,values)
+    return sql
+    return execute(sql)
 
-    try:
-        cursor.execute(sql)
-        conn.commit()
-        rowcount = cursor.rowcount
-    except Exception as e:
-        print sql
-        print e
-        rowcount = -1
-
-    if rowcount == 0:
-        return '没有找到对应的行'
-    elif rowcount == -1:
-        return '插入失败'
-    else:
-        return '插入成功'
-
-
+#only need id parameter
 def delete_data(sql,req):
     try:
         sql = sql + ' where id="%s"'%req.GET['id']
     except:
         return '没有指定要删除的ID'
+    return execute(sql)
 
+def execute(sql):
     try:
         cursor.execute(sql)
         conn.commit()
-        rowcount = cursor.rowcount
+        return '没有找到对应的行' if cursor.rowcount==0 else '执行成功'
     except Exception as e:
-        print sql
-        print e
-        rowcount = -1
-
-    if rowcount == 0:
-        return '没有找到对应的行'
-    elif rowcount == -1:
-        return '删除失败'
-    else:
-        return '删除成功'
-
+        return u'执行失败<br>SQL:%s<br>Exception:%s'%(sql,str(e))
 
 #format return data with json
 def format_table(column,data):
